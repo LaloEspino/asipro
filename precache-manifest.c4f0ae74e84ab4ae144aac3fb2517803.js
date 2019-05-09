06240c9874d87741f991872d89afa55b3a9f0105
Eduardo Espino <lalospn@gmail.com>
2019-05-08 23:03:43 -0500

    Updates

---
+++ precache-manifest.c4f0ae74e84ab4ae144aac3fb2517803.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d507c3cb4905a6eb6dfcc8a421a7541",
+    "url": "/asipro/index.html"
+  },
+  {
+    "revision": "5eb207f3aa1c3c571593",
+    "url": "/asipro/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e7ca2f5bca1fe9fc05eb",
+    "url": "/asipro/static/css/main.43ca1fff.chunk.css"
+  },
+  {
+    "revision": "5eb207f3aa1c3c571593",
+    "url": "/asipro/static/js/2.afd7426c.chunk.js"
+  },
+  {
+    "revision": "e7ca2f5bca1fe9fc05eb",
+    "url": "/asipro/static/js/main.c2731539.chunk.js"
+  },
+  {
+    "revision": "98a23d6d1ff305da56e0",
+    "url": "/asipro/static/js/runtime~main.b7fb00ec.js"
+  },
+  {
+    "revision": "a898e5d169f6e6ef6aa9076a971d9891",
+    "url": "/asipro/static/media/bug.a898e5d1.svg"
+  },
+  {
+    "revision": "f2b216d3772931453e3598b6a005bf33",
+    "url": "/asipro/static/media/clientes.f2b216d3.png"
+  },
+  {
+    "revision": "0c3ab62c48e1a4a7d1e84722f1204c45",
+    "url": "/asipro/static/media/curriculum.0c3ab62c.pdf"
+  },
+  {
+    "revision": "fcc77119f78fdc72e45a62d56e1f753e",
+    "url": "/asipro/static/media/logo.fcc77119.svg"
+  },
+  {
+    "revision": "9159431c7308b203822f110b5fb379a7",
+    "url": "/asipro/static/media/pc1404.9159431c.pdf"
+  },
+  {
+    "revision": "efdf919484cbb041940c0d2925d8c35f",
+    "url": "/asipro/static/media/pc1404.efdf9194.jpg"
+  },
+  {
+    "revision": "0b2714c58f8036f599b9322ba6378693",
+    "url": "/asipro/static/media/pc1616.0b2714c5.pdf"
+  },
+  {
+    "revision": "4601bb68a56e83f6980e012f4af71f79",
+    "url": "/asipro/static/media/pc1616.4601bb68.jpg"
+  },
+  {
+    "revision": "0b2714c58f8036f599b9322ba6378693",
+    "url": "/asipro/static/media/pc1832.0b2714c5.pdf"
+  },
+  {
+    "revision": "10f3b1de5efa23a2adb166d92a34b58d",
+    "url": "/asipro/static/media/pc1832.10f3b1de.jpg"
+  }
+]);
